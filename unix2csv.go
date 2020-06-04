456d5f29f1902932ca369903dc5bb787f4ee088f
gorematol <mattgore78@googlemail.com>
2020-06-04 22:25:35 +0100

    fixed bugs with interface traversal plus code tidy up with fmt

--- unix2csv.go
+++ unix2csv.go
@@ -1,87 +1,74 @@
-package main 
+package main
 
 import (
-    "fmt"
-    "log"
-    "os"
-//    "bufio"
-    "io/ioutil"
-    "encoding/json"
-//    "reflect"
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
 )
 
-
 func main() {
+	type File struct {
+		Input     string
+		Delimeter string
+		Output    string
+	}
 
-    // var csvd string = ","
-    type File struct {
-        Input string 
-	Delimeter string
-	Output string  
-    }
+	type Unixfiles struct {
+		Files []File
+	}
 
-    type Unixfiles struct  {
-	Files []File 
-    }
+	// Open json file
+	jsonfile, err := openFile("unixmeta.json")
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
 
-    // Open json file
-    jsonfile, err := openFile("unixmeta.json")
-    if err != nil {
-	log.Fatal(err)
-        os.Exit(1) 
-    }
+	// Read json file into a byte array
+	b, err := ioutil.ReadAll(jsonfile)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
 
-    // Read json file into a byte array
-    b, err := ioutil.ReadAll(jsonfile)
-    if err != nil {
-	log.Fatal(err)
-        os.Exit(1) 
-    }
-
- //   metadata := Unixfiles{}
- //   json.Unmarshal(b, &metadata)
-    processData(Unixfiles{}, b)
-    jsonfile.Close()
+	processData(Unixfiles{}, b)
+	jsonfile.Close()
 }
 
 func openFile(filename string) (*os.File, error) {
-        f, err := os.Open(filename)
-        if err != nil {
-		return nil, err 
-        }
-        return f, nil
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	return f, nil
 }
 
 func processData(metadata interface{}, b []byte) {
+	var csvd = ","
+	json.Unmarshal(b, &metadata)
+	m := metadata.(map[string]interface{})
 
-      json.Unmarshal(b, &metadata)
-      m := metadata.(map[string]interface{}) 
-      fmt.Println(m) 
-
-      for _,value := range m {
-	      for _,file := range value.([]interface{}) {
-		      input, err := file["Input"]
-		      fmt.Println(input)
-	      }
-      }
-      /* 
-      file, err := openFile(value.Input)
-      if err != nil {
-	log.Fatal(err)
-        os.Exit(1) 
-      }
-
-      lineoutput := bufio.NewScanner(file)
-      for lineoutput.Scan() {
-//	if strings.Contains(lineoutput.Text(), d) {
-//            newstr := strings.Replace(lineoutput.Text(), d,  csvd, -1)
-//            fmt.Println(newstr)
-        fmt.Println(lineoutput.Text())
-//        }
-      }
-      file.Close()
-    }
-      */
-
-}    
-
+	for _, value := range m {
+		for _, files := range value.([]interface{}) {
+			record := files.(map[string]interface{})
+			inputfile, err := openFile(record["Input"].(string))
+			if err != nil {
+				log.Fatal(err)
+				os.Exit(1)
+			}
+			d := record["Delimeter"].(string)
+			lineoutput := bufio.NewScanner(inputfile)
+			for lineoutput.Scan() {
+				if strings.Contains(lineoutput.Text(), d) {
+					newstr := strings.Replace(lineoutput.Text(), d, csvd, -1)
+					fmt.Println(newstr)
+				}
+			}
+			inputfile.Close()
+		}
+	}
+}
